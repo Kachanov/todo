b6eef751475e11c6f72be6174e5be1a1a37f0e2c
Unknown <mr.kachanov@gmail.com>
2018-01-22 09:56:17 +0200

    Added input and "add_button"

--- src/index.js
+++ src/index.js
@@ -2,8 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello() {
-    return <h1>Hi</h1>
+let todoList = [];
+
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {item: ""};
+    }
+
+    handleChange = (event) => {
+        this.setState({item: event.currentTarget.value});
+        console.log(event.currentTarget.value);
+    };
+
+    handleClick = (event) => {
+        todoList.push(this.state.item);
+        console.log(todoList);
+        var input = document.getElementById("input");
+        input.value = "";
+    };
+
+    render() {
+        return(
+            <div>
+                <input onChange={this.handleChange} className="input" id="input"/>
+                <button onClick={this.handleClick} className="addButton" id="addButton"> Add </button>
+            </div>
+        );
+    }
+
+
 }
 
-ReactDOM.render(<Hello/>, document.getElementById('root'));
+ReactDOM.render(<Input />, document.getElementById('root'));
