65446b3044d73e98a69293d0b9524f08fdaa1a6e
Unknown <mr.kachanov@gmail.com>
2018-02-01 16:13:47 +0200

    Implemented corrent todos deleting

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -20,62 +20,60 @@ const initialState = (function () {
 })();
 
 export function todoList(state = initialState, action) {
-    if(action.type === "ADD_TODO") {
-        return {
-            ...state,
-            todoArray: [
-                ...state.todoArray,
-                {
-                    value: action.payload,
-                    done: false,
-                    id: state.todoArray.length
-                }
-            ]
-        }
+    switch (action.type) {
+        case "ADD_TODO":
+            return {
+                ...state,
+                todoArray: [
+                    ...state.todoArray,
+                    {
+                        value: action.payload,
+                        done: false,
+                        id: state.todoArray.length
+                    }
+                ]
+            };
+
+        case "DELETE_TODO":
+            let index = action.payload;
+            state.todoArray.splice(index, 1);
+            state.todoArray.forEach(function (todo, index) {
+                todo.id = index;
+            });
+
+            return {
+                ...state, todoArray: [...state.todoArray]
+            };
+
+        case "DONE_TODO":
+            index = action.payload;
+            state.todoArray[index].done = true;
+
+            return {
+                ...state, todoArray: [...state.todoArray]
+            };
+
+        case "SHOW_DONE":
+            state.currentFilter = state.filters[1];
+
+            return {
+                ...state, currentFilter: state.currentFilter
+            };
+
+        case "SHOW_CURRENT":
+            state.currentFilter = state.filters[0];
+
+            return {
+                ...state, currentFilter: state.currentFilter
+            };
+
+        case "SHOW_ALL":
+            state.currentFilter = state.filters[2];
+
+            return {
+                ...state, currentFilter: state.currentFilter
+            };
+        default:
+            return state;
     }
-
-    if(action.type === "DELETE_TODO") {
-        let index = action.payload;
-        state.todoArray[index] = {};
-
-        return {
-            ...state, todoArray: [...state.todoArray]
-        }
-
-    }
-
-    if(action.type === "DONE_TODO") {
-        let index = action.payload;
-        state.todoArray[index].done = true;
-
-        return {
-            ...state, todoArray: [...state.todoArray]
-        }
-    }
-
-    if(action.type === "SHOW_DONE") {
-        state.currentFilter = state.filters[1];
-
-        return {
-            ...state, currentFilter: state.currentFilter
-        }
-    }
-
-    if(action.type === "SHOW_CURRENT") {
-        state.currentFilter = state.filters[0];
-
-        return {
-            ...state, currentFilter: state.currentFilter
-        }
-    }
-
-    if(action.type === "SHOW_ALL") {
-        state.currentFilter = state.filters[2];
-
-        return {
-            ...state, currentFilter: state.currentFilter
-        }
-    }
-
-    return state;
 }
